80b086e80825781cb4e24d017f80b97369e15ad7
oilegor1029 <rogelioramos96@gmail.com>
2017-10-11 16:06:08 +0200

    Lazyload template commit

--- addon/components/light-box.js
+++ addon/components/light-box.js
@@ -4,7 +4,7 @@ import layout from '../templates/components/light-box';
 export default Ember.Component.extend({
 	layout: layout,
 	tagName: 'a',
-	attributeBindings: ['href', 'data-lightbox', 'data-title', 'data-class'],
+	attributeBindings: ['href', 'lazyload-src', 'data-lightbox', 'data-title', 'data-class'],
 	inlineImage: true,
 	classNames: ['ember-lightbox'],
 	classNameBindings: ['inlineImage']
